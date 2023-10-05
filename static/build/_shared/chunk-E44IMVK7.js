8238c6016dc609ce44d96650d4b701575170ad43
Ethan Yu <ethayu1@gmail.com>
2023-10-05 01:28:18 -0400

    5

--- static/build/_shared/chunk-E44IMVK7.js
+++
@@ -1,7 +0,0 @@
-// app/tailwind.css
-var tailwind_default = "/build/_assets/tailwind-LOHQCNDA.css";
-
-export {
-  tailwind_default
-};
-//# sourceMappingURL=/build/_shared/chunk-E44IMVK7.js.map
--- static/build/_shared/chunk-YQVIYTPS.js
+++
@@ -1,7 +0,0 @@
-// app/tailwind.css
-var tailwind_default = "/build/_assets/tailwind-LKTN5O7D.css";
-
-export {
-  tailwind_default
-};
-//# sourceMappingURL=/build/_shared/chunk-YQVIYTPS.js.map
